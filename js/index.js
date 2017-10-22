9b4cc01cb001c5f8eeb9ef2a4d3d6220e241f6b6
freshtang <11514376@qq.com>
2017-10-22 13:16:49 +0800

    Site updated: 2017-10-22 13:16:49

--- js/index.js
+++ js/index.js
@@ -12,3 +12,24 @@ tog.addEventListener('click', function (e) {
         wrap.style.height = li.length * 40 + 'px';
     }
 })
+
+// 侧边栏按钮
+var togside = document.querySelector('#tog-side');
+togside.addEventListener('click', function (e) {
+    var sidebar = document.querySelector('.sidebar');
+    var body = document.querySelector('body');
+    var icon = document.querySelector('#tog-side #tog-icon');
+    if (sidebar.classList.contains('active')) {
+        sidebar.classList.remove('active');
+        body.classList.remove('open-side');
+
+        icon.classList.remove('fa-reply');
+        icon.classList.add('fa-user');
+    } else {
+        sidebar.classList.add('active');
+        body.classList.add('open-side');
+
+        icon.classList.remove('fa-user');
+        icon.classList.add('fa-reply');
+    }
+})
